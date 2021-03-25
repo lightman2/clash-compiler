25b0c5ad011aed2ace355cac270f95843047f1b3
Alex McKenna <alex@qbaylogic.com>
2021-03-25 17:13:44 +0100

    Bit and BitVector primitives

--- clash-ghc/src-ghc/Clash/GHC/PartialEval/Eval.hs
+++ clash-ghc/src-ghc/Clash/GHC/PartialEval/Eval.hs
@@ -68,6 +68,8 @@ import           Clash.Debug (debugIsOn, traceM)
 import           Clash.Driver.Types (Binding(..), IsPrim(..))
 import qualified Clash.Normalize.Primitives as NP (undefined)
 
+import           Clash.GHC.PartialEval.Primitive.Bit
+import           Clash.GHC.PartialEval.Primitive.BitVector
 import           Clash.GHC.PartialEval.Primitive.ByteArray
 import           Clash.GHC.PartialEval.Primitive.Char
 import           Clash.GHC.PartialEval.Primitive.Double
@@ -351,7 +353,9 @@ evalPrimitive pr args = do
       pure (VNeutral (NePrim pr forcedArgs))
  where
   primitives = HashMap.unions
-    [ byteArrayPrims
+    [ bitPrims
+    , bitVectorPrims
+    , byteArrayPrims
     , charPrims
     , doublePrims
     , enumPrims
